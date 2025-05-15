302d4d9b8ab7e768f04a8709e2a9d3c0421807db
robertstow <roberts@hyve.com>
2025-05-15 22:15:44 +0100

    feat: check if user before adding

--- routes/users.mjs
+++ routes/users.mjs
@@ -6,40 +6,49 @@ const router = express.Router();
 
 // Get list of users
 router.get("/", async (req, res) => {
-    let collection = await db.collection("users");
-    let results = await collection.find({}).limit(50).toArray();
+  let collection = await db.collection("users");
+  let results = await collection.find({}).limit(50).toArray();
 
-    res.send(results).status(200);
+  res.send(results).status(200);
 });
 
 // Get single user
 router.get("/:id", async (req, res) => {
-    let collection = await db.collection("users");
-    let query = { clerkID: req.params.id };
-    console.log(collection);
+  let collection = await db.collection("users");
+  let query = { clerkID: req.params.id };
+  console.log(collection);
 
-    let result = await collection.findOne(query);
+  let result = await collection.findOne(query);
 
-    if (!result) res.send("Not found").status(404);
-    else res.send(result).status(200);
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
 });
 
 // Add new user
 router.post("/", async (req, res) => {
+  try {
     let collection = await db.collection("users");
-    let newDocument = req.body;
-    let result = await collection.insertOne(newDocument);
-    res.send(result).status(200);
+    const { clerkID, forces } = req.body;
+    const existingUser = await collection.findOne({ clerkID });
+    if (existingUser) {
+      return res.status(200).send({ message: "User already exists", user: existingUser });
+    }
+    const result = await collection.insertOne({ clerkID, forces });
+    return res.status(201).send({ message: "User created", result });
+  } catch (error) {
+    console.error("Error creating user:", err);
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
 });
 
 // Delete user
 router.delete("/:id", async (req, res) => {
-    const query = { _id: ObjectId(req.params.id) };
+  const query = { _id: ObjectId(req.params.id) };
 
-    const collection = db.collection("users");
-    let result = await collection.deleteOne(query);
+  const collection = db.collection("users");
+  let result = await collection.deleteOne(query);
 
-    res.send(result).status(200);
+  res.send(result).status(200);
 });
 
 export default router;
